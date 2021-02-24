33086c9d824aa5b6d9ec28de7db6ac68665832b8
jessicadelgrande <jessica.delgrande@gmail.com>
2021-02-24 10:42:41 -0500

    minor text update; removed unused fonts

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,63 +1,63 @@
 "use strict";
 
 // smooth scroll (gsap)
-var html = document.documentElement;
-var body = document.body;
-var scroller = {
-	target: document.querySelector("#scroll-container"),
-	ease: 0.1, // scroll speed
-	endY: 0,
-	y: 0,
-	resizeRequest: 1,
-	scrollRequest: 0
-};
+// const html = document.documentElement;
+// const body = document.body;
+// const scroller = {
+// 	target: document.querySelector("#scroll-container"),
+// 	ease: 0.1, // scroll speed
+// 	endY: 0,
+// 	y: 0,
+// 	resizeRequest: 1,
+// 	scrollRequest: 0,
+// };
 
-var requestId = null;
+// let requestId = null;
 
-gsap.set(scroller.target, {
-	rotation: 0.01,
-	force3d: true
-});
+// gsap.set(scroller.target, {
+// 	rotation: 0.01,
+// 	force3d: true
+// });
 
-function updateScroller() {
-	var resized = scroller.resizeRequest > 0;
+// function updateScroller() {
+// 	const resized = scroller.resizeRequest > 0;
 
-	if (resized) {
-		var height = scroller.target.clientHeight;
-		body.style.height = height + "px";
-		scroller.resizeRequest = 0;
-	}
+// 	if (resized) {
+// 		const height = scroller.target.clientHeight;
+// 		body.style.height = height + "px";
+// 		scroller.resizeRequest = 0;
+// 	}
 
-	var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+// 	const scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
 
-	scroller.endY = scrollY;
-	scroller.y += (scrollY - scroller.y) * scroller.ease;
+// 	scroller.endY = scrollY;
+// 	scroller.y += (scrollY - scroller.y) * scroller.ease;
 
-	if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-		scroller.y = scrollY;
-		scroller.scrollRequest = 0;
-	}
+// 	if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+// 		scroller.y = scrollY;
+// 		scroller.scrollRequest = 0;
+// 	}
 
-	gsap.set(scroller.target, {
-		y: -scroller.y
-	});
+// 	gsap.set(scroller.target, {
+// 		y: -scroller.y
+// 	});
 
-	requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-}
+// 	requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+// }
 
-function onScroll() {
-	scroller.scrollRequest++;
-	if (!requestId) {
-		requestId = requestAnimationFrame(updateScroller);
-	}
-}
+// function onScroll() {
+// 	scroller.scrollRequest++;
+// 	if (!requestId) {
+// 		requestId = requestAnimationFrame(updateScroller);
+// 	}
+// }
 
-function onResize() {
-	scroller.resizeRequest++;
-	if (!requestId) {
-		requestId = requestAnimationFrame(updateScroller);
-	}
-}
+// function onResize() {
+// 	scroller.resizeRequest++;
+// 	if (!requestId) {
+// 		requestId = requestAnimationFrame(updateScroller);
+// 	}
+// }
 
 // toggles class on menu
 // manages display of selected content
